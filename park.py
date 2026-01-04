371843443fb07898fe25ed4253719ca115fa7335
sagarshinde511 <amplifierelectronics9@gmail.com>
2026-01-04 10:59:10 +0530

    Update park.py

--- park.py
+++ park.py
@@ -205,7 +205,7 @@ if not st.session_state.logged_in:
 
 # ================= DASHBOARD =================
 else:
-    #st_autorefresh(interval=20000, key="refresh")
+    st_autorefresh(interval=20000, key="refresh")
 
     st.title("🚗 Smart Parking Dashboard")
     st.write(f"👤 {st.session_state.username} ({st.session_state.role})")
